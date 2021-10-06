69ddba7370c442890e48add0bbe165292732c41c
ryan flitcroft <ryanflitcroft@ymail.com>
2021-10-06 15:40:20 -0700

    animal sounds app

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // import functions
+const catImage = document.getElementById('cat-image');
+const horseImage = document.getElementById('horse-image');
+const dogImage = document.getElementById('dog-image');
 
+const catSound = document.getElementById('cat-sound');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
 // reference needed DOM elements
+
+catImage.addEventListener('click', () => {
+    catSound.play();
+})
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+})
+
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+})
 
 
 // set event listeners 
